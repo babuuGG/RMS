f88f42361e559a795d02ebdf4b32f7ce2aa70324
srian2 <sandeepkandel45@gmail.com>
2024-02-27 16:07:53 +0545

    coding for admin login updated

---
+++ admin_login.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+
+
+
+
+
+root = Tk()
+root.title("Admin User")
+root.geometry("500x500")
+root.configure(bg='yellow')
+
+
+
+register_admin = Frame(root)
+register_admin.pack()
+
+Label(register_admin, text="Welcome Admin", font=("Helvetica", 20)).pack(pady=10)
+
+Label(register_admin, text="Username").pack(pady=5)
+E1 = Entry(register_admin, width=30)
+E1.pack(pady=5)
+
+Label(register_admin, text="Admin Password").pack(pady=5)
+E2 = Entry(register_admin, width=30,show="*")
+E2.pack(pady=5)
+
+submit_btn = Button(register_admin, text="SUBMIT")
+submit_btn.pack(pady=5)
+
+root.mainloop()
